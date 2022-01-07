a09757b9983552e4af27250d44feb79946ea3a48
Dimas Eka Adinandra <42790532+dimascapella@users.noreply.github.com>
2022-01-07 12:13:36 +0700

    add: init commit golang app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dimascapella/app-say-hello.git
+
+go 1.17
+
+require github.com/dimascapella/go-module/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	go_module "github.com/dimascapella/go-module/v2"
+)
+
+func main() {
+	fmt.Println(go_module.HelloWorld())
+	fmt.Println(go_module.SayHello("Badar"))
+}
